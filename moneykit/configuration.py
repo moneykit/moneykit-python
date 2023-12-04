85a7bb3123995e50114734d759da6ddd7ad68522
moneykitt <dev+github-moneykitt-bot@moneykit.com>
2023-12-04 17:43:42 +0000

    Fix publish

--- moneykit/configuration.py
+++ moneykit/configuration.py
@@ -406,7 +406,9 @@ class Configuration:
             "OS: {env}\n"
             "Python Version: {pyversion}\n"
             "Version of the API: 0.1.0\n"
-            "SDK Package Version: 0.0.1".format(env=sys.platform, pyversion=sys.version)
+            "SDK Package Version: 0.1.0.alpha.1".format(
+                env=sys.platform, pyversion=sys.version
+            )
         )
 
     def get_host_settings(self):
