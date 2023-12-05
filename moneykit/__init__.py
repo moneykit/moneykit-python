dbd9bf109c15d16d17b4305d3083bb53722c59a7
moneykitt <dev+github-moneykitt-bot@moneykit.com>
2023-12-05 15:24:48 +0000

    Update with latest OpenAPI changes

--- moneykit/__init__.py
+++ moneykit/__init__.py
@@ -14,7 +14,7 @@
 """  # noqa: E501
 
 
-__version__ = "0.1.0.alpha.1"
+__version__ = "0.1.0.alpha.2"
 
 # import apis into sdk package
 from moneykit.api.access_token_api import AccessTokenApi
--- moneykit/configuration.py
+++ moneykit/configuration.py
@@ -406,7 +406,7 @@ class Configuration:
             "OS: {env}\n"
             "Python Version: {pyversion}\n"
             "Version of the API: 0.1.0\n"
-            "SDK Package Version: 0.1.0.alpha.1".format(
+            "SDK Package Version: 0.1.0.alpha.2".format(
                 env=sys.platform, pyversion=sys.version
             )
         )
